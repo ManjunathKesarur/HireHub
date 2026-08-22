f901917b21926c4a3e16687375a434b6f51158f7
ManjunathKesarur <manjunath.dev05@gmail.com>
2026-08-22 20:21:40 +0530

    Add method to retrieve all job applications

--- src/main/java/com/lancer/HireHub/controller/JobApplicationController.java
+++ src/main/java/com/lancer/HireHub/controller/JobApplicationController.java
@@ -1,6 +1,9 @@
 package com.lancer.HireHub.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,5 +23,10 @@ JobApplicationService applicationService;
 	@PostMapping
 	public JobApplication applyjob(@RequestBody	JobApplication jobApplication) {
 		return applicationService.applyjob(jobApplication);
+	}
+	
+	@GetMapping
+	public List<JobApplication> getAllApplication(JobApplication application) {
+		return applicationService.getAllApplication(application);
 	}
 }
